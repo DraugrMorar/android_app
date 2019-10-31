9f61fc6da628b632036eb4aa18d6c2f9e3f0911e
Tatiana Radnaeva <radnaevatatianaz@gmail.com>
2019-10-31 12:07:27 +0300

    smth new

---
+++ app/src/main/java/com/example/myapplication/MActivity.java
@@ -0,0 +1,77 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MActivity extends Activity {
+
+    private EditText task;
+    private EditText date;
+    private Button add;
+    private ItemsAdapter adapter;
+
+
+    static class Item {
+        String task;
+        String date;
+
+        public Item(String task, String date) {
+            this.task = task;
+            this.date = date;
+        }
+
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        task = (EditText) findViewById(R.id.task);
+        date = (EditText) findViewById(R.id.data);
+        add = (Button) findViewById(R.id.add);
+        adapter = new ItemsAdapter();
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                putText();
+            }
+        };
+        add.setOnClickListener(listener);
+    }
+
+    private void putText() {
+        if (task.getText().length() == 0) {
+            return;
+        } else {
+            ListView items = (ListView) findViewById(R.id.items);
+            items.setAdapter(adapter);
+                adapter.add(new Item(task.getText().toString(), date.getText().toString()));
+        }
+    }
+
+    private class ItemsAdapter extends ArrayAdapter<Item> {
+
+        public ItemsAdapter() {
+            super(MActivity.this, R.layout.item);
+        }
+
+        @Override
+        @NonNull
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            final View view = getLayoutInflater().inflate(R.layout.item, null);
+            final Item item = getItem(position);
+            if (item.task.length() == 0) {
+                return view;
+            }
+            ((TextView) view.findViewById(R.id.task)).setText(item.task);
+            ((TextView) view.findViewById(R.id.data)).setText(item.date);
+            return view;
+        }
+    }
+}
