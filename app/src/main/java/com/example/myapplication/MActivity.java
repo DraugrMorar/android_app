ffa0ccb3ad57de13e88de033eee47e541bf8ee46
OUT-Marchuk-AA <out-marchuk-aa@cab-wsm-0002136.sigma.sbrf.ru>
2019-11-05 10:25:51 +0300

    second activity

--- app/src/main/java/com/example/myapplication/MActivity.java
+++ app/src/main/java/com/example/myapplication/MActivity.java
@@ -11,41 +11,36 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class MActivity extends Activity {
-
-    private EditText task;
-    private EditText date;
-    private Button add;
     private Button second_activity;
-    private ItemsAdapter adapter;
 
 
-    static class Item {
-        String task;
-        String date;
-
-        public Item(String task, String date) {
-            this.task = task;
-            this.date = date;
-        }
-
-    }
+//    static class Item {
+//        String task;
+//        String date;
+//
+//        public Item(String task, String date) {
+//            this.task = task;
+//            this.date = date;
+//        }
+//
+//    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        task = (EditText) findViewById(R.id.task);
-        date = (EditText) findViewById(R.id.data);
-        add = (Button) findViewById(R.id.add);
+//        task = (EditText) findViewById(R.id.task);
+//        date = (EditText) findViewById(R.id.data);
+//        add = (Button) findViewById(R.id.add);
         second_activity = (Button) findViewById(R.id.secondA);
-        adapter = new ItemsAdapter();
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                putText();
-            }
-        };
-        add.setOnClickListener(listener);
+//        adapter = new ItemsAdapter();
+//        View.OnClickListener listener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                putText();
+//            }
+//        };
+//        add.setOnClickListener(listener);
 
         View.OnClickListener swap = new View.OnClickListener() {
             @Override
@@ -58,34 +53,5 @@ public class MActivity extends Activity {
         };
         second_activity.setOnClickListener(swap);
 
-    }
-    private void putText() {
-        if (task.getText().length() == 0) {
-            return;
-        } else {
-            ListView items = (ListView) findViewById(R.id.items);
-            items.setAdapter(adapter);
-                adapter.add(new Item(task.getText().toString(), date.getText().toString()));
-        }
-    }
-
-    private class ItemsAdapter extends ArrayAdapter<Item> {
-
-        public ItemsAdapter() {
-            super(MActivity.this, R.layout.item);
-        }
-
-        @Override
-        @NonNull
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            final View view = getLayoutInflater().inflate(R.layout.item, null);
-            final Item item = getItem(position);
-            if (item.task.length() == 0) {
-                return view;
-            }
-            ((TextView) view.findViewById(R.id.task)).setText(item.task);
-            ((TextView) view.findViewById(R.id.data)).setText(item.date);
-            return view;
-        }
     }
 }
--- app/src/main/java/com/example/myapplication/secondActivity.java
+++ app/src/main/java/com/example/myapplication/secondActivity.java
@@ -1,14 +1,40 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+
 
 public class secondActivity extends AppCompatActivity {
+
+//    private EditText task;
+//    private EditText date;
+//    private Button add;
+   // private ItemsAdapter adapter;
+
+//    static class Item {
+//        String task;
+//        String date;
+//
+//        public Item(String task, String date) {
+//            this.task = task;
+//            this.date = date;
+//        }
+//
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
     }
+
+
 }
+
+
